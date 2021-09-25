73c4f63d833b6f1fe20c91749ced8b56b47c186a
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-09-25 17:11:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let video_player = document.getElementById("video_player");
+let links = video_player.getElementsByTagName('a');
+for(var i=0; i<links.length; i++){
+  links[i].onclick = handler;
+}
+
+function handler(e){
+  
+  e.preventDefault();
+  
+let videotarget = this.getAttribute("href");
+let filename = videotarget.substr(0,videotarget.lastIndexOf('.'))||videotarget;
+let video = document.querySelector("#video_player video");
+  
+video.removeAttribute('poster');
+video.load();
+video.play();
+  
+let source = document.querySelectorAll("#video_player video source");
+   source[0].src = filename + ".mp4";
+   source[1].src = filename + ".m3u8";
+  
+ /* video.buffer();*/
+}
+handler();
